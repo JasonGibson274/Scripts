22261fa0ae0844b7a2cabbc3596025fb304085a5
Jason Gibson <jgibson37@gatech.edu>
2017-11-07 23:52:45 -0500

    leave in only well behaved programs

--- setup.bash
+++ setup.bash
@@ -3,28 +3,28 @@
 
 
 # init 0
-#bash move.bash 0&
+#bash ~/Scripts/move.bash 0&
 
 # init 1
-bash move.bash 1&
+bash ~/Scripts/move.bash 1&
 sleep 0.1
 emacs -mm&
 disown
 sleep 1
 
 # init 5
-bash move.bash 5&
+bash ~/Scripts/move.bash 5&
 sleep 0.1
 firefox&
 disown
 sleep 1
 
 # init
-bash move.bash 4&
+bash ~/Scripts/move.bash 4&
 sleep 0.1
 slack&
 disown
 sleep 0.1
-evolution
-disown
-sleep 0.1
+#evolution
+#disown
+#sleep 0.1
