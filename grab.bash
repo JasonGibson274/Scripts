7097156c842682070aae7f542ad1147aab02f982
Jason Gibson <jgibson37@gatech.edu>
2018-02-08 18:25:32 -0500

    moved things around

--- grab.bash
+++ grab.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cp --parents ~/.emacs.d/init.el all/.emacs
+cp ~/.emacs.d/init.el all/.emacs/.
 cp ~/.editorconfig all/.
-cp --parents ~/.i3/config all/.i3
+cp ~/.i3/config all/.i3/.
 cp ~/.bashrc all/.
