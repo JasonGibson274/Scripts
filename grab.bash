0c348b2373996140bf7dad21308d2326336d04ee
Jason Gibson <jgibson37@gatech.edu>
2018-09-01 16:55:12 -0400

    updated scripts

--- grab.bash
+++ grab.bash
@@ -5,3 +5,7 @@ cp ~/.editorconfig all/.
 cp ~/.i3/config all/.i3/.
 cp ~/.bashrc all/.
 cp /etc/i3status.conf all/etc/.
+cp ~/.xintrc all/.
+cp ~/.Xmodmap all/.
+cp ~/.xsession all/.
+cp ~/.xsessionrc all/.
